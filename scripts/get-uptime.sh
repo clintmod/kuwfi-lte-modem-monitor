a2947a82e217b7e020ff6db394be87f73603f475
Clint M <cmodien@gmail.com>
2020-10-26 21:43:19 -0600

    Removing line endings only needed for macos

--- scripts/get-uptime.sh
+++ scripts/get-uptime.sh
@@ -4,11 +4,10 @@ set -e
 
 mkdir -p workdir
 
-CR=$(printf "\r")
 set +e
 ./scripts/get-uptime.tcl $1 2>&1 > /dev/null || true
 set -e
-REGEX="s#\.[0-9]*${CR}##g"
+REGEX="s#\.[0-9]*##g"
 
 if grep -q "/proc/uptime" workdir/$1.uptime.txt; then
   cat workdir/$1.uptime.txt | tail -n 3 | head -n 1 | awk '{print $1}' | sed $REGEX
